f16583011a553c41d33c85c0445288c91d6b3e8e
Ifigeneia Giannakoudi <igiannakoudi@perimeterinstitute.ca>
2023-01-16 15:08:24 -0500

    fixed mult and added things

--- src/DualNumbers.jl
+++ src/DualNumbers.jl
@@ -24,5 +24,25 @@ Base.:-(x::Dual) = x + -y
 Base.:*(a::Number, x::Dual) = Dual(a * x.real, a* x.dual)
 Base.:*(x:: Dual, a:: Number) = Dual(x.real* a, x.dual* a)
 
+#multiplication
+
+#	x = a + eps b
+#	y = c + eps d
+#	eps^2 = 0
+#
+#	x * y = a*c + eps * (a*d + c*b)
+Base.:*(x::Dual, y::Dual) = Dual(x.real * y.real, x.real * y.dual + x.dual* y.real)
+#conjugate
+Base.conj(x::Dual) = Dual(x.real,-x.dual)
+
+#inverse
+#	a/(a + eps b)
+#	=(a - eps b)/ [(a + eps b)(a - eps b)]
+#	=(a - eps b) * (1/a^2)
+Base.inv(x::Dual) = conj(x) * (1 / x.real^2)
+#division
+Base.:/(x::Dual, y::Dual)= x * inv(y)
+
+
 end # module DualNumbers
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -10,5 +10,5 @@ y = Dual(1, 3)
 @test x+y == Dual(2,5)
 @test x + - y == Dual(0,-1)
 @test 4*x == Dual(4,8) 
-@test y*8 == Dual(8,40) 
+@test y*8 == Dual(8,24) 
 
