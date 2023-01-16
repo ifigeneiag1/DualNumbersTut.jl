0c5f2bb939bf38e7c07081e8298c33af72d46897
Ifigeneia Giannakoudi <igiannakoudi@perimeterinstitute.ca>
2023-01-16 14:52:13 -0500

    multiplication not working

--- src/DualNumbers.jl
+++ src/DualNumbers.jl
@@ -3,15 +3,26 @@ module DualNumbers
 export Dual
 
 """
-     Dual: A dual number
+     Dual: A dual number 
 """
 struct Dual{T}
    real::T
    dual::T
 end
 
+# identity and negation
+Base.:+(x::Dual) = Dual(+x.real, +x.dual)
+Base.:-(x::Dual) = Dual(-x.real,-x.dual)
 Base.:+(x::Dual, y::Dual) = Dual(x.real + y.real, x.dual + y.dual)
-Base.:-(x::Dual, y::Dual) = Dual(x.real - y.real, x.dual - y.dual)
 
+#addition and subtraction operators
+#Base.:-(x::Dual, y::Dual) = Dual(x.real - y.real, x.dual - y.dual)
+Base.:+(x::Dual, y::Dual) = Dual(x.real + y.real, x.dual + y.dual)
+Base.:-(x::Dual) = x + -y
+
+# scalar multiplication
+Base.:*(a::Number, x::Dual) = Dual(a * x.real, a* x.dual)
+Base.:*(x:: Dual, a:: Number) = Dual(x.real* a, x.dual* a)
 
 end # module DualNumbers
+
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,11 @@ using Test
 x = Dual(1, 2)
 y = Dual(1, 3)
 
-z= x + y
-@test z == Dual(2,5)
+@test +x == x
+@test -x == Dual(-1,-2)
+
+@test x+y == Dual(2,5)
+@test x + - y == Dual(0,-1)
+@test 4*x == Dual(4,8) 
+@test y*8 == Dual(8,40) 
 
